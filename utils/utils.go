a6782fd01417fdb85934f0a1b94c690809108787
vcarey <val@adelphic.com>
2022-07-24 13:25:17 -0400

    modified time utility

--- utils/utils.go
+++ utils/utils.go
@@ -2,34 +2,29 @@ package utils
 
 import (
 	"fmt"
-	"github.com/viant/toolbox"
 	"strings"
 	"time"
 )
 
 func StringToTime(ts string) *time.Time {
-	layout := "2006-01-02 15:04:05.000"
+	layout := "2006-01-02 15:04:05.000-07"
 	if strings.Contains(ts, "T") {
 		layout = time.RFC3339Nano
-	} else {
-		layout = "2006-01-02 15:04:05.000-07"
 	}
-	t, err := toolbox.ToTime(ts, layout)
+	t, err := time.Parse(layout[0:len(ts)], ts)
 	if err != nil {
 		fmt.Println("failed to convert time: %s %v\n", ts, err)
 		return nil
 	}
-	return t
+	return &t
 }
 
 func StringToTimeMs(ts string) int64 {
-	var layout string
+	layout := "2006-01-02 15:04:05.000-07"
 	if strings.Contains(ts, "T") {
 		layout = time.RFC3339Nano
-	} else {
-		layout = "2006-01-02 15:04:05.000-07"
 	}
-	t, err := toolbox.ToTime(ts, layout)
+	t, err := time.Parse(layout[0:len(ts)], ts)
 	if err != nil {
 		fmt.Println("failed to convert time: %s %v\n", ts, err)
 		return 0
@@ -44,14 +39,11 @@ func TimeMsToString(millis int64) string {
 }
 
 func StringToDate(ts string) int32 {
-	layout := "2006-01-02"
+	layout := "2006-01-02 15:04:05.000-07"
 	if strings.Contains(ts, "T") {
 		layout = time.RFC3339Nano
-	} else {
-		layout = "2006-01-02 15:04:05.000-07"
 	}
-
-	t, err := toolbox.ToTime(ts, layout)
+	t, err := time.Parse(layout[0:len(ts)], ts)
 	if err != nil {
 		fmt.Println("failed to convert time: %s %v\n", ts, err)
 		return 0
